6cacae530a19a3815641e67682610eb126666221
Bartosz Wilk <b.wilk@cyfronet.pl>
2016-03-31 11:37:03 +0200

    Added some filters to sevices schema

--- schemas/service.js
+++ schemas/service.js
@@ -3,11 +3,14 @@ module.exports = {
     additionalProperties: true,
     list: {
         filters: {
-            sitename: ["sitename"]
+            sitename: ["sitename"], 
+            type: ["type"], 
+            sitename_type: ["sitename", "type"]
         },
         value: {
             sitename: "sitename",
             hostname: "hostname", 
+            type: "type"
         }
     },
     properties: {
